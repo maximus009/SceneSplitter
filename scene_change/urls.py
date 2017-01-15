4eb50ecd6aa5612dbc277bc376f333d41849acae
Kalpathy Sitaraman Sivaraman <ks6cq@recognition.cs.virginia.edu>
2017-01-15 12:54:33 -0500

    Updated settings

--- scene_change/urls.py
+++ scene_change/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.autodiscover()
 urlpatterns = [
     # Examples:
     # url(r'^$', 'scene_change.views.home', name='home'),
